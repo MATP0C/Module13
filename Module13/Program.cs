aece842a6090c85f6437cae373a9c44320d4028f
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-07-02 20:11:35 +0300

    13.5.8

--- Module13/Program.cs
+++ Module13/Program.cs
@@ -1,47 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace StackTest
 {
     class Program
     {
-        public static Stack<string> words = new Stack<string>();
+        public static ConcurrentQueue<string> words = new ConcurrentQueue<string>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в стек.");
+            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в очередь.");
             Console.WriteLine();
+
+            StartQueueProcessing();
 
             while (true)
             {
                 var input = Console.ReadLine();
 
-                switch (input)
+                if(input == "peek")
                 {
-                    case "Pop":
-                        {
-                            words.TryPop(out string popResult);
-                            break;
-                        }
-                    
-                    case "Peek":
-                        {
-                            words.TryPeek(out string peekResult);
-                            break;
-                        }
-                        words.Push(input);
-                        break;
-                }
-
-
-                Console.WriteLine();
-                Console.WriteLine("В стеке:");
-
-                foreach (var word in words)
-                {
-                    Console.WriteLine(" " + word);
+                    if(words.TryPeek(out var elem))
+                    {
+                        Console.WriteLine(elem);
+                    }
+                    else
+                    {
+                        words.Enqueue(input);
+                    }
                 }
             }
+        }
+
+        static void StartQueueProcessing()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                while (true)
+                {
+                    Thread.Sleep(5000);
+                    if (words.TryDequeue(out var element))
+                        Console.WriteLine("======>  " + element + " прошел очередь");
+                }
+
+            }).Start();
         }
     }
 }
