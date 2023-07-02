51610ee52ef81fd14b063be97dddb7fb679075b3
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-07-02 14:59:13 +0300

    13.3.12

--- Module13/Program.cs
+++ Module13/Program.cs
@@ -6,21 +6,25 @@ namespace CountWords
     {
         static void Main(string[] args)
         {
-            var sentence = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
-            var characters = sentence.ToCharArray();
-            var symbols = new HashSet<char>();
-            foreach (var symbol in characters)
-                symbols.Add(symbol);
-            Console.WriteLine(symbols.Count);
+            while (true)
+            {
+                Console.WriteLine("Введите текст:");
+                var sentence = Console.ReadLine();
+                var characters = sentence.ToCharArray();
+                var symbols = new HashSet<char>();
+                foreach (var symbol in characters)
+                    symbols.Add(symbol);
+                Console.WriteLine(symbols.Count);
 
-            var signs = new[] { ',', ' ', '.' };
-            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                var signs = new[] { ',', ' ', '.' };
+                var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-            bool containsNumbers = symbols.Overlaps(numbers);
-            Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
+                bool containsNumbers = symbols.Overlaps(numbers);
+                Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
 
-            symbols.ExceptWith(signs);
-            Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
+                symbols.ExceptWith(signs);
+                Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
+            }
         }
     }
 }
