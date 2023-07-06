28baf7b493cd687e0f273dc8d0cf22c2c32505fd
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-07-06 09:32:50 +0300

    Задание 13.6.1 (HW-03)

--- Module13/Program.cs
+++ Module13/Program.cs
@@ -3,37 +3,53 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Module13
 {
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            List<string> list = new List<string>();
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+
             string path = @"C:\\Users\\bogda\\OneDrive\\Рабочий стол\\C#\\Grob.txt";
             string text = File.ReadAllText(path);
             char[] delimiterChars = { ' ', ',', '.', ':', '!', '?', '№', '‰', '(', '±', ')', '‡', '“', '”', '\t' };
-            Dictionary<string, int> wordCount = new Dictionary<string, int>();
             string[] words = text.Split(delimiterChars);
 
-            foreach (string word in words)
-                if (!wordCount.ContainsKey(word))
-                {
-                    wordCount.Add(word, 1);
-                }
-                else
-                {
-                    int count = wordCount[word];
-                    wordCount[word] = count + 1;
-                }
 
-            string[] top10Words = wordCount.OrderByDescending(x => x.Value).Take(10).Select(x => x.Key).ToArray();
-            Console.WriteLine($"Топ-10 самых часто повторяющихся слов:");
-            for(int i = 0; i < top10Words.Length; i++)
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+
+            for (int i = 0; i < words.Length; i++)
             {
-                Console.WriteLine("{0} : {1}", top10Words[i], wordCount[top10Words[i]]);
+                list.Add(words[i]);
             }
-            Console.ReadKey();
+
+            stopWatch.Stop();
+            Console.WriteLine($"Время вставки элементов в список: {stopWatch.ElapsedMilliseconds} миллисекунд");
+
+
+            list.Clear();
+            linkedList.Clear();
+
+
+            stopWatch = new Stopwatch();
+            stopWatch.Restart();
+
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                linkedList.AddLast(words[i]);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"Время вставки элементов в связанный список: {stopWatch.ElapsedMilliseconds} миллисекунд");
         }
     }
 }
